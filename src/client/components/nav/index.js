36cdcb5037a804e8b596b9950a089acb32512878
Tamas Sule <admin@tamassule.com>
2018-01-18 12:41:27 +0100

    Fixes #47 Handles different formats of the author field in package.json

--- src/client/components/nav/index.js
+++ src/client/components/nav/index.js
@@ -1,14 +1,36 @@
 import { h } from "preact";
 import "./style.scss";
-let Nav = ({ name = "", version = "NaN", makers = "" }) => (
-  <nav className="nav">
-    <ul>
-      <li className="project">{name}</li>
-      <li className="version">{version}</li>
-      <li className="makers">{makers}</li>
-      <li className="snippets chuckright">snippets</li>
-    </ul>
-  </nav>
-);
+let Nav = ({
+  name = "",
+  version = "NaN",
+  makers = { name: "", email: "", url: "" }
+}) => {
+  const emailLink = makers.email ? (
+    <a href={`mailto:${makers.email}`}>{makers.email}</a>
+  ) : (
+    ""
+  );
+  const webLink = makers.url ? (
+    <a target="_blank" href={makers.url}>
+      {makers.url}
+    </a>
+  ) : (
+    ""
+  );
+  return (
+    <nav className="nav">
+      <ul>
+        <li className="project">{name}</li>
+        <li className="version">{version}</li>
+        <li className="makers">
+          <span>{makers.name}</span>
+          {emailLink}
+          {webLink}
+        </li>
+        <li className="snippets chuckright">snippets</li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
