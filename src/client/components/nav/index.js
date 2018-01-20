c2f61784bbde3adb76445a4cb0e1ed004d8f1124
Pickra <picker10024@yahoo.com>
2018-01-20 00:28:50 -0500

    Made nav work with smaller sizes + less code

--- src/client/components/nav/index.js
+++ src/client/components/nav/index.js
@@ -1,35 +1,38 @@
 import { h } from "preact";
 import "./style.scss";
+
 let Nav = ({
   name = "",
   version = "NaN",
   makers = { name: "", email: "", url: "" }
 }) => {
   const emailLink = makers.email ? (
-    <a href={`mailto:${makers.email}`}>{makers.email}</a>
+    <a href={`mailto:${makers.email}`} className="nav-link">
+      {makers.email}
+    </a>
   ) : (
     ""
   );
+
   const webLink = makers.url ? (
-    <a target="_blank" href={makers.url}>
+    <a target="_blank" href={makers.url} className="nav-link">
       {makers.url}
     </a>
   ) : (
     ""
   );
+
   return (
-    <nav className="nav">
-      <ul>
-        <li className="project">{name}</li>
-        <li className="version">{version}</li>
-        <li className="makers">
-          <span>{makers.name}</span>
-          {emailLink}
-          {webLink}
-        </li>
-        <li className="snippets chuckright">snippets</li>
-      </ul>
-    </nav>
+    <ul className="nav">
+      <li className="project">{name}</li>
+      <li className="version">{version}</li>
+      <li className="makers">
+        <span>{makers.name}</span>
+        {emailLink}
+        {webLink}
+      </li>
+      <li className="snippets">snippets</li>
+    </ul>
   );
 };
 
