4557c2471eb650c3acc97312c5f79372e2d2df3b
Zouhir <zouhir@zouhir.org>
2018-01-22 00:06:00 +1100

    wip varibales restructure

--- src/client/components/board.js
+++ src/client/components/board.js
@@ -64,7 +64,10 @@ export default class Board extends Component {
       if (data.message.toLowerCase() !== "idle") {
         this.setState({
           progress: data,
-          logs: [`<p>${data.message}</p>`, `<p>${(data.percentage * 100).toFixed(2)}%</p>`]
+          logs: [
+            `<p>${data.message}</p>`,
+            `<p>${(data.percentage * 100).toFixed(2)}%</p>`
+          ]
         });
       }
     });
