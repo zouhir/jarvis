e91968712e17b9d7f7e4121f46c714dc25a0b70f
Luke Edwards <luke.edwards05@gmail.com>
2018-02-01 18:04:03 -0800

    use class syntax

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -6,88 +6,91 @@ const authors = require("parse-authors");
 
 const pkg = importFrom(process.cwd(), "./package.json");
 
-function Jarvis(opts = {}) {
-  opts.host = opts.host || "localhost";
-  opts.port = parseInt(opts.port || 1337, 10);
+class Jarvis {
+  constructor(opts = {}) {
+    opts.host = opts.host || "localhost";
+    opts.port = parseInt(opts.port || 1337, 10);
 
-  if (opts.port && isNaN(opts.port)) {
-    console.error(`[JARVIS] error: the specified port (${opts.port}) is invalid. Reverting to 1337`);
-    opts.port = 1337;
+    if (opts.port && isNaN(opts.port)) {
+      console.error(`[JARVIS] error: the specified port (${opts.port}) is invalid. Reverting to 1337`);
+      opts.port = 1337;
+    }
+
+    this.options = opts;
+
+    this.env = {
+      production: false,
+      running: false, // indicator if our express server + sockets are running
+      watching: false
+    };
+
+    this.reports = {
+      stats: {},
+      progress: {},
+      project: {}
+    };
   }
 
-  this.options = opts;
+  apply(compiler) {
+    const { name, version, author: makers } = pkg;
+    const normalizedAuthor = parseAuthor(makers);
 
-  this.env = {
-    production: false,
-    running: false, // indicator if our express server + sockets are running
-    watching: false
-  };
+    this.reports.project = { name, version, makers: normalizedAuthor };
 
-  this.reports = {
-    stats: {},
-    progress: {},
-    project: {}
-  };
+    // check if the current build is production, via defined plugin
+    const definePlugin = compiler.options.plugins.find(fn => fn.constructor.name === "DefinePlugin");
+
+    if (definePlugin) {
+      const pluginNodeEnv = definePlugin["definitions"]["process.env.NODE_ENV"];
+      this.env.production = pluginNodeEnv === "production";
+    }
+
+    if (!this.env.running) {
+      server.start(this.options, () => {
+        this.env.running = true;
+        // if a new client is connected push current bundle info
+        server.io.on("connection", socket => {
+          socket.emit("project", this.reports.project);
+          socket.emit("progress", this.reports.progress);
+          socket.emit("stats", this.reports.stats);
+        });
+      });
+    }
+
+    compiler.plugin("watch-run", (c, done) => {
+      this.env.watching = true;
+      done();
+    });
+
+    compiler.plugin("run", (c, done) => {
+      this.env.watching = false;
+      done();
+    });
+
+    // report the webpack compiler progress
+    compiler.apply(
+      new webpack.ProgressPlugin((percentage, message) => {
+        this.reports.progress = { percentage, message };
+        server.io.emit("progress", { percentage, message });
+      })
+    );
+
+    // extract the final reports from the stats!
+    compiler.plugin("done", stats => {
+      const jsonStats = stats.toJson({ chunkModules: true });
+      jsonStats.isDev = !this.env.production;
+      this.reports.stats = reporter.statsReporter(jsonStats);
+      server.io.emit("stats", this.reports.stats);
+
+      if (!this.env.watching) {
+        server.close();
+      }
+    });
+
+    // that's it!
+  }
 }
 
-Jarvis.prototype.apply = function(compiler) {
-  const { name, version, author: makers } = pkg;
-  const normalizedAuthor = parseAuthor(makers);
-
-  this.reports.project = { name, version, makers: normalizedAuthor };
-
-  // check if the current build is production, via defined plugin
-  const definePlugin = compiler.options.plugins.find(fn => fn.constructor.name === "DefinePlugin");
-
-  if (definePlugin) {
-    const pluginNodeEnv = definePlugin["definitions"]["process.env.NODE_ENV"];
-    this.env.production = pluginNodeEnv === "production";
-  }
-
-  if (!this.env.running) {
-    server.start(this.options, () => {
-      this.env.running = true;
-      // if a new client is connected push current bundle info
-      server.io.on("connection", socket => {
-        socket.emit("project", this.reports.project);
-        socket.emit("progress", this.reports.progress);
-        socket.emit("stats", this.reports.stats);
-      });
-    });
-  }
-
-  compiler.plugin("watch-run", (c, done) => {
-    this.env.watching = true;
-    done();
-  });
-
-  compiler.plugin("run", (c, done) => {
-    this.env.watching = false;
-    done();
-  });
-
-  // report the webpack compiler progress
-  compiler.apply(
-    new webpack.ProgressPlugin((percentage, message) => {
-      this.reports.progress = { percentage, message };
-      server.io.emit("progress", { percentage, message });
-    })
-  );
-
-  // extract the final reports from the stats!
-  compiler.plugin("done", stats => {
-    const jsonStats = stats.toJson({ chunkModules: true });
-    jsonStats.isDev = !this.env.production;
-    this.reports.stats = reporter.statsReporter(jsonStats);
-    server.io.emit("stats", this.reports.stats);
-
-    if (!this.env.watching) {
-      server.close();
-    }
-  });
-
-  // that's it!
-};
 
 const parseAuthor = function(author) {
   if (typeof author === "string") {
